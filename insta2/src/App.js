3e8b760c5bcd13e656a6a43ee85721b5b0cada40
Jarrad Miller <jm020801@gmail.com>
2018-11-26 15:17:55 -0700

    day1

--- insta2/src/App.js
+++ insta2/src/App.js
@@ -1,28 +1,51 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import dummyData from './dummy-data';
+import PostContainer from './components/PostContainer/PostContainer';
+import SearchBar from './components/SearchBar/SearchBar';
 
 class App extends Component {
-  render() {
+  constructor() {
+    super();
+    this.state = {
+      posts: dummyData,
+      inputText: ''
+  }
+  }
+   handleChange = event => {
+    this.setState({
+      inputText: event.target.value
+    })
+  }
+   addComment = event => {
+    event.preventDefault();
+    this.setState({
+      // data: [...this.state.data, 
+      // {myComment: this.state.inputText,
+      // username: 'Bob'}],
+      // data: this.state.data.map(e => {
+      //   if (e.timestamp === event.timestamp) {
+      //     console.log('timestamp', event.timestamp)
+      //   }
+      // }),
+      inputText: ''
+    })
+  }
+   render() {
+    // console.log('data is...', this.state.data[0].comments)
+    // console.log('data is...', this.state.data)
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-          
-          </a>
-        </header>
+        <div>
+          <SearchBar />
+        </div>
+        <div>
+            <PostContainer 
+            posts={this.state.posts}
+            />
+        </div>
       </div>
     );
   }
 }
-
 export default App;
---
+++ insta2/src/components/PostContainer/PostContainer.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import CommentSection from '../CommentSection/CommentSection';
+import './PostContainer.css'
+
+import Post from './Post';
+ const PostContainer = props => {
+    return (
+        <div className='instagram-card'>
+             <div className='card-text'>
+                <div>
+                    {props.posts.map(p => 
+                    <Post key={p.imageUrl} post={p}/>)}
+                    {/* Here we send state data one by one to Post */}
+                </div>
+            </div>
+        </div>
+    )
+}
+ export default PostContainer;
